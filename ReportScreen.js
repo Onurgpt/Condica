dad32c65c44d89b1fbe72069f84b6bcc2bd828a7
Onurgpt <onursalkim2@gmail.com>
2025-08-15 09:14:41 +0300

    Create ReportScreen.js

---
+++ ReportScreen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function ReportScreen() {
+  return (
+    <View style={{ padding: 20 }}>
+      <Text>Attendance Reports will be generated here.</Text>
+    </View>
+  );
+}
